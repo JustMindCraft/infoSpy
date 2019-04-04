6bdf078a7ff9dc24afe225993199fe4be54e759f
simon <xsqfeater@gmail.com>
2019-04-04 08:24:11 +0800

    hanpeidou.lif

--- gunDB/config.js
+++ gunDB/config.js
@@ -4,13 +4,13 @@ const config ={
         "https://simontaosim.herokuapp.com/gun",
         "https://gun.hanpeidou.life/gun",
         "https://hanpei.herokuapp.com/gun",
-        "https://gun.10000cars.com/gun",
+        // "https://gun.10000cars.com/gun",
         // "http://gun2.hanpeidou.life/gun"
         // 测试开发用http
     ],
-    rootId: "taosim.net"
+    // rootId: "taosim.net"
     // // rootId: "lododor_com_pro"
-    // rootId: "hanpeidou.life"
+    rootId: "hanpeidou.life"
     // rootId: "8kas.com"
 }
 
